587023c77d27b5216eb1cb38a02eef62ea7fc0a2
Abhijeet Raj <104703498+AbhijeetRaj646@users.noreply.github.com>
2025-04-24 11:55:31 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 import Preloader from "./components/Preloader";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
 import Projects from "./components/Projects/Projects";
-import Footer from "./components/Footer";
 import Resume from "./components/Resume/ResumeNew";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Footer from "./components/Footer";
+import ScrollToTop from "./components/ScrollToTop";
+
 import "./style.css";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-
-import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   const [load, updateLoad] = useState(true);
@@ -20,18 +21,17 @@ function App() {
     const timer = setTimeout(() => {
       updateLoad(false);
     }, 1200);
-
     return () => clearTimeout(timer);
   }, []);
 
   return (
     <Router basename="/portfolio">
+      <ScrollToTop />
       {load ? (
         <Preloader load={load} />
       ) : (
         <div className="App" id={load ? "no-scroll" : "scroll"}>
           <Navbar />
-          <ScrollToTop />
           <Switch>
             <Route exact path="/" component={Home} />
             <Route path="/project" component={Projects} />
@@ -45,4 +45,4 @@ function App() {
   );
 }
 
-export default App; 
+export default App;
